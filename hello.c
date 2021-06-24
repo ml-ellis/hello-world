2544d488d6b2a885aa0d03ffcdd94ff2db8e7ee7
ml-ellis <ellismadeline22@gmail.com>
2021-06-23 20:47:11 -0400

    upload

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  puts("Pretty bird says: 'Hello World! I is very cute, fluffy, sassy, pretty, green, and brave.'");
+  getchar();
+  return EXIT_SUCCESS;
+}
